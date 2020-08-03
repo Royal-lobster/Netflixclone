6fe9d1af89341be5e052c09b94c937275634f5f6
Royal-lobster <srujangs8@gmail.com>
2020-08-03 22:22:13 +0530

    built all categories and showing on frontend

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import React from 'react';
-import './App.css';
-
+import React from "react";
+import "./App.css";
+import Row from "./Row";
+import requests from "./request";
 function App() {
   return (
-    <div className="App">
-      <h1>hello</h1>
-      <Row title="NETFLIX ORIGINALS" />
-      <Row title="Trending Now" />
+    <div className="app">
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
+      />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
     </div>
   );
 }
